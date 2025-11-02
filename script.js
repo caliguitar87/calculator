6ee7fb5e8bd18921b71dcd251af1e6e387fc93cc
Krystal <235606157+caliguitar87@users.noreply.github.com>
2025-11-02 07:42:43 -0800

    Added the style to the calculator. Added the delete and dot function to the calculator. Fixed bugs in the delete button.

--- script.js
+++ script.js
@@ -15,18 +15,19 @@ function operator(operators, x, y) {
 
     if (operators == '/') {
         if (Number(y) != 0) {
-            return Math.round(divide(x,y));
+            const factor = 10**2;
+            return Math.round(factor * divide(x, y)) / factor;
         }
     }
     else if (operators == '*') {
-        return multiply(x,y);
+        return multiply(x, y);
     }
     else if (operators == '-') {
-        return subtract(x,y);
+        return subtract(x, y);
 
     }
     else if (operators == '+') {
-        return add(x,y);
+        return add(x, y);
 
     }
 
@@ -41,77 +42,107 @@ let body = document.querySelector('body');
 let seven = document.querySelector('.seven').addEventListener('click',
     () => {
         num += 7;
-        console.log(num);
+        display.textContent = num;
         return num;
     }
 );
 let eight = document.querySelector('.eight').addEventListener('click', () => {
     num += 8;
-    console.log(num);
+    display.textContent = num;
     return num;
 });
 let nine = document.querySelector('.nine').addEventListener('click', () => {
     num += 9;
-    console.log(num);
+    display.textContent = num;
     return num;
 });
 let four = document.querySelector('.four').addEventListener('click', () => {
     num += 4;
-    display.textContent=num;
+    display.textContent = num;
     return num;
 });
 let five = document.querySelector('.five').addEventListener('click', () => {
     num += 5;
-    display.textContent=num;
+    display.textContent = num;
     return num;
 });
 let six = document.querySelector('.six').addEventListener('click', () => {
     num += 6;
-    display.textContent=num;
+    display.textContent = num;
     return num;
 });
 let one = document.querySelector('.one').addEventListener('click', () => {
     num += 1;
-    display.textContent=num;
+    display.textContent = num;
     return num;
 });
 let two = document.querySelector('.two').addEventListener('click', () => {
     num += 2;
-    display.textContent=num;
+    display.textContent = num;
     return num;
 });
 let three = document.querySelector('.three').addEventListener('click', () => {
     num += 3;
-    display.textContent=num;
+    display.textContent = num;
     return num;
 });
 let zero = document.querySelector('.zero').addEventListener('click', () => {
     num += 0;
-    display.textContent=num;
+    display.textContent = num;
     return num;
 });
+let dot = document.querySelector('.dot').addEventListener('click', () => {
+    if(!num.includes(".")){
+    num += '.';
+    display.textContent = num;
+    return num;}
+});
+let del = document.querySelector('.del').addEventListener('click', () => {
+    let temp =num.slice(0,-1);
+    num = temp;
+    if (!num){
+        num='';
+        result =0;
+        numOne='';
+        numTwo='';
+    }
+        display.textContent=0;
+        return num;
+    
+
+   }
+
+
+    
+);
 let display = document.querySelector('.output');
 let equals = document.querySelector('.equals').addEventListener('click', () => {
-    display.textContent =operator(operators,Number(num),Number(numOne));
+
+    result= operator(operators, Number(numOne), Number(num));
+    display.textContent=result;
+    return result;
+    
+    
 });
-let clear = document.querySelector('.clear').addEventListener('click',()=>{
-    num='';
-    numOne='';
-    numTwo='';
-    result=0;
-    display.textContent=0;
+let clear = document.querySelector('.clear').addEventListener('click', () => {
+    num = '';
+    numOne = '';
+    numTwo = '';
+    result = 0;
+    display.textContent = result;
 })
 let divid = document.querySelector('.divide').addEventListener('click', () => {
-     if (numOne == '') {
+    if (numOne == '') {
         numOne = num;
         num = '';
-        operators ='/';
+        operators = '/';
     }
     else {
-        numTwo=num;
+        numTwo = num;
         if (operators != '') {
             if (numTwo != '') {
                 result = operator(operators, Number(numOne), Number(numTwo));
+                display.textContent = result;
                 numOne = result;
                 operators = '/';
                 num = '';
@@ -119,18 +150,20 @@ let divid = document.querySelector('.divide').addEventListener('click', () => {
             }
         }
     }
-console.log(result);});
+    console.log(result);
+});
 let mult = document.querySelector('.multiply').addEventListener('click', () => {
-     if (numOne == '') {
+    if (numOne == '') {
         numOne = num;
         num = '';
-        operators ='*';
+        operators = '*';
     }
     else {
-        numTwo=num;
+        numTwo = num;
         if (operators != '') {
             if (numTwo != '') {
                 result = operator(operators, Number(numOne), Number(numTwo));
+                display.textContent = result;
                 numOne = result;
                 operators = '*';
                 num = '';
@@ -138,18 +171,20 @@ let mult = document.querySelector('.multiply').addEventListener('click', () => {
             }
         }
     }
-console.log(result);})
+    console.log(result);
+})
 let sub = document.querySelector('.subtract').addEventListener('click', () => {
-      if (numOne == '') {
+    if (numOne == '') {
         numOne = num;
         num = '';
-        operators ='-';
+        operators = '-';
     }
     else {
-        numTwo=num;
+        numTwo = num;
         if (operators != '') {
             if (numTwo != '') {
                 result = operator(operators, Number(numOne), Number(numTwo));
+                display.textContent = result;
                 numOne = result;
                 operators = '-';
                 num = '';
@@ -157,19 +192,21 @@ let sub = document.querySelector('.subtract').addEventListener('click', () => {
             }
         }
     }
-console.log(result);}
+    console.log(result);
+}
 );
 let sums = document.querySelector('.sum').addEventListener('click', (e) => {
     if (numOne == '') {
         numOne = num;
         num = '';
-        operators ='+';
+        operators = '+';
     }
     else {
-        numTwo=num;
+        numTwo = num;
         if (operators != '') {
             if (numTwo != '') {
                 result = operator(operators, Number(numOne), Number(numTwo));
+                display.textContent = result;
                 numOne = result;
                 operators = '+';
                 num = '';
@@ -177,6 +214,7 @@ let sums = document.querySelector('.sum').addEventListener('click', (e) => {
             }
         }
     }
-console.log(result);}
+    console.log(result);
+}
 );
 
